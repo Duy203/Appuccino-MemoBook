1a2d92387127ab5f2c357c31d1f52532dbb859ef
Duy203 <duy.bao.nguyen@bellevuecollege.edu>
2025-05-21 00:59:06 -0700

    basic login screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,24 @@
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { View, Text, Button } from "react-native";
 
 export default function Home() {
   return (
     <View style={{ padding: 20 }}>
-      <Text style={{ fontSize: 24 }}>Home Screen</Text>
+      <Text style={{ fontSize: 24, marginBottom: 20 }}>Home Screen</Text>
+
       <Link href="/about" asChild>
         <Button title="Go to About Page" />
       </Link>
+
+      <View style={{ marginTop: 20 }}>
+        <Button
+          title="Logout"
+          color="red"
+          onPress={() => {
+            router.replace("/login"); // Navigate back to login screen
+          }}
+        />
+      </View>
     </View>
   );
-};
+}
