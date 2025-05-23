5e3f79fb8380a5f7a0b71a9c58ab5654b05fbd78
seansmgoutlook <seansmg@outlook.com>
2025-05-22 20:38:17 -0700

    Fixed errors on index.tsx and tsconfig.json and got the android app to work on VS studio code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Link, router } from "expo-router";
-import { View, Text, Button } from "react-native";
+import React from "react";
+import { Button, Text, View } from "react-native";
 
 export default function Home() {
   return (
